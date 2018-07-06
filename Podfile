06330483d5ad77a539cedd14af997facb5e69e33
guoguangtao <jude_guo@163.com>
2018-07-06 17:53:01 +0800

    工程搭建

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '7.0'
+inhibit_all_warnings!
+
+target 'FMDBHandler' do
+
+	pod 'FMDB'
+
+end
---
+++ Pods/Target Support Files/Pods-FMDBHandler/Pods-FMDBHandler-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FMDBHandler : NSObject
+@end
+@implementation PodsDummy_Pods_FMDBHandler
+@end
